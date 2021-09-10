93bf6f5f698ee910f1e9df4d18a3becdbbdfeaae
Mark24 <mark.zhangyoung@qq.com>
2021-09-11 00:21:07 +0800

    init

---
+++ lib/sinatrapp.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "sinatrapp/version"
+
+module Sinatrapp
+  class Error < StandardError; end
+  # Your code goes here...
+  puts "Hello Sinatrapp"
+end
